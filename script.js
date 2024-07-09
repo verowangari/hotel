f159c5ed15e8b36487cf8767215c3546704c822b
verowangari <verowangari34@gmail.com>
2024-07-09 15:36:58 +0300

    test javascript

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function myFunction(p1,p2)
 {
     return p1*p2;
 }
-let result= myFunction(7,8);
-document.getElementById('button').innerHTML=result;
+// let result= myFunction(7,8);
+let text ="The product of 7 and 8 is " + myFunction(7,8)
+document.getElementById('button').innerHTML=text;
